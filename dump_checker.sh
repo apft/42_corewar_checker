1ed7ace299fe457407c29e1809166166ceb1d571
JK <kettani.jalil@gmail.com>
2019-07-26 05:02:55 +0200

    Modify code to accept multiple players in dump_checker

--- dump_checker.sh
+++ dump_checker.sh
@@ -5,14 +5,22 @@
 # 3: player
 # 4: cycle
 # 5: verbose level
+
 run_test(){
-	$1 -v $5 -d $4 $3 > vm1_output.tmp 2>&1
-	$2 -v $5 -d $4 $3 > vm2_output.tmp 2>&1
+	local exec1=$1
+	local exec2=$2
+	local cycle=$3
+	local verbose_lvl=$4
+	shift 4
+	local players=$@
+
+	$exec1 -v $verbose_lvl -d $cycle $players > vm1_output.tmp 2>&1
+	$exec2 -v $verbose_lvl -d $cycle $players > vm2_output.tmp 2>&1
 	diff vm1_output.tmp vm2_output.tmp > diff_output.tmp
 }
 
 run_tests(){
-	local nb_of_cycles=`$VM1_EXEC -v 2 $PLAYER | grep "It is now cycle" | tail -n 1 | cut -d ' ' -f 5 | bc`
+	local nb_of_cycles=`$VM1_EXEC -v 2 $PLAYERS | grep "It is now cycle" | tail -n 1 | cut -d ' ' -f 5 | bc`
 	local min=1
 	local max=$nb_of_cycles
 	
@@ -20,7 +28,7 @@ run_tests(){
 	while [ $min -lt `echo "$max - 1" | bc` ];
 	do
 		cycle=`echo "($min + $max) / 2" | bc`
-		run_test $VM1_EXEC $VM2_EXEC $PLAYER $cycle 0
+		run_test $VM1_EXEC $VM2_EXEC $cycle 0 $PLAYERS
 		if [ -s diff_output.tmp ];then
 			max=$cycle
 		else
@@ -31,12 +39,13 @@ run_tests(){
 		printf "Same dump output\n"
 	else
 		printf "Dump differs at cycle %d\n" $max
-		run_test $VM1_EXEC $VM2_EXEC $PLAYER $max 31
+		run_test $VM1_EXEC $VM2_EXEC $max 31 $PLAYERS
 	fi
 }
 
 VM1_EXEC=$1
 VM2_EXEC=$2
-PLAYER=$3
+shift 2
+PLAYERS=$@
 
 run_tests
